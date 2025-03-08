f92a7fc7b2a1e7c6a441d9567a85a3314ad6b34a
Jonathan Curtis <jon.curtis.94@gmail.com>
2025-03-08 21:54:22 +0100

    Updates are working but dynamical instabilities are still wrong

--- AL_vortex_dynamics.py
+++ AL_vortex_dynamics.py
@@ -84,14 +84,14 @@ def update_vortices(n,Js,Jp,muvdtdx):
 		### Fictious boundary densities 
 		nL = 0.
 		nR = 0.
-		n_out[i,0] = n[i,0] - (n[i,1] - n[i,0])*v[1] - (n[i,0]- nL)*float(v[0]<0.)
+		n_out[i,0] = n[i,0] - (n[i,1] - n[i,0])*v[1] - v[0]*n[i,0]*float(v[0]<0.)
 
 		### Now for the bulk which can have unimpeded flow 
 		for x in range(1,len(n[i,:])-1):
 			n_out[i,x] = n[i,x] - (n[i,x+1] - n[i,x])*v[x+1] - (n[i,x] - n[i,x-1])*v[x]
 
 		### Now for the right boundary
-		n_out[i,-1] = n[i,-1] - (n[i,-1] - n[i,-2])*v[-2] - (nR - n[i,-1])*v[-1]*float(v[-1] > 0.) 
+		n_out[i,-1] = n[i,-1] - (n[i,-1] - n[i,-2])*v[-2] + n[i,-1]*v[-1]*float(v[-1] > 0.) 
 
 	### Now we annihilate overlapping densities
 	### We do such that if n+ > n- we take n+ -> n+- n- and n- -> 0 and vice versa if n- > n+.
@@ -131,9 +131,10 @@ def biot_savart_kernel(sites,links,lPearl):
 
 	return kernel 
 
+### This method computes the inverse Biot-Savart kernel needed for the current updates 
+### Can be run once per geometry 
+def inverse_kernel(kernel):	
 
-### This method inverts the Biot-Savart kernel to find the current from the vortex dustribution and total current 
-def invert_kernel(kernel,nv,Jtot):
 	### The kernel relates the current to the vortex density by 
 	### Kernel * J = nv
 	### However the current is defined on links of which there are N+1 and the vorticity is defined on sites of which there are N
@@ -145,72 +146,65 @@ def invert_kernel(kernel,nv,Jtot):
 	square_kernel[:-1,:] = kernel 
 	square_kernel[-1,:] = 1./float(N+1)*np.ones(N+1)
 
-	rhs = np.zeros(N+1)
-	rhs[:-1] = nv
-	rhs[-1] = Jtot
+	return np.linalg.inv(square_kernel)
 
-	return np.linalg.inv(square_kernel)@rhs
+### This method inverts the Biot-Savart kernel to find the current from the vortex dustribution and total current 
+def update_current(inv_kernel,n,Jav):
+
+	N = inv_kernel.shape[0] ### This is the number of links 
+
+	rhs = np.zeros(N) 
+	rhs[:-1] = n[0,:] - n[1,:] ### only couples the difference between density of vortices and antivortices
+	
+	rhs[-1] = Jav
+
+	return inv_kernel@rhs
+
+
+
 
 def main():
 	W = 20.*um ### Sample width
 	d = .05*um ### Sample thickness
 	l = .15*um ### London penetration depth
 	lPearl = 2.*l**2/d ### Pearl length 2lambda^2/d
-	muv = (.04*um/ps)/Jc ### Vortex mobility, specified by drift velocity at depairing current 
+	muv = 10.*(.04*um/ps)/Jc ### Vortex mobility, specified by drift velocity at depairing current 
 	Jp = 0.05*Jc ### We take depinning current to be 5% of the depairing current 
 
 	### Vortices live on sites and reside at -W/2 + dx, -W/2 + 2dx, ..., W/2-dx
 	### Currents live on links and reside at -W/2 + dx/2, -W/2 + 3dx/2, ..., W/2-dx/2
-	N = 30
+	N = 100
 	sites, links = gen_lattices(W,N)
 	dx = sites[1] - sites[0]
 
-	kernel = biot_savart_kernel(sites,links,lPearl)
-	
-	### Supercurrent density profile 
-	### Supercurrent lives on the links 
-	Js = 1.2*np.cosh(links*2./W)/np.cosh(1.)*Jp
-
-	plt.plot(links,Js,color='purple',marker='.')
-	plt.axhline(Jp,color='gray',linestyle='dashed')
-	plt.xlabel(r'$x$ [$\mu$m]')
-	plt.ylabel(r'$J_s/J_c$')
-	plt.show()
-
-	nvs = kernel@Js
-
-	Js = invert_kernel(kernel,nvs,1.3*Jp)
-
-	plt.plot(sites,kernel@Js,color='red',marker='x')
-	plt.xlabel(r'$x$ [$\mu$m]')
-	plt.ylabel(r'$\phi_0 n_v$ [$\phi/\mu$m$^2$]')
-	plt.show()
-
-	plt.plot(links,Js,color='purple',marker='.')
-	plt.axhline(Jp,color='gray',linestyle='dashed')
-	plt.xlabel(r'$x$ [$\mu$m]')
-	plt.ylabel(r'$J_s/J_c$')
-	plt.show()
-
-
-	quit()
-
-
-	nts = 1000
-	times = np.linspace(0.,10.*ps,nts)
+	nts = 500
+	times = np.linspace(0.,50.*ps,nts)
 	dt = times[1]-times[0]
-	nvs = np.zeros((nts,2,len(sites)))
 
-	#nvs[0,0,:] = np.maximum(-0.01*np.sign(2.*np.pi*sites/W),0.)
-	#nvs[0,1,:] = np.maximum(0.01*np.sign(2.*np.pi*sites/W),0.)
-	nvs[0,0,:] = np.maximum(np.array([ 0.01*float(np.abs(x) < 2.*um) for x in sites ]),0.)
-	nvs[0,1,:] = np.zeros_like(nvs[0,0,:])
+	kernel = biot_savart_kernel(sites,links,lPearl)
+	invkernel = inverse_kernel(kernel)
+	
+	Jav = 1.5*Jp
+	
+	nvs = np.zeros((nts,2,len(sites))) 
+	Js = np.zeros((nts,len(links)))
+
+	nvs[0,0,N//3] = 0.1
+	nvs[0,1,2*N//3] = 0.1
+	Js[0,:] = update_current(invkernel,nvs[0,:,:],Jav)
+
+	### Initial current distribution without vortices
+	plt.plot(links,Js[0,:],color='purple')
+	plt.axhline(Jp,color='gray',linestyle='dashed')
+	plt.xlabel(r'$x$ [$\mu$m]')
+	plt.ylabel(r'$J_s/J_c$')
+	plt.show()
 
 	for i in range(1,nts):
-		nvs[i,:,:] = update_vortices(nvs[i-1,:,:],Js,Jp,muv*dt/dx)
+		nvs[i,:,:] = update_vortices(nvs[i-1,:,:],Js[i-1,:],Jp,muv*dt/dx)
+		Js[i,:] = update_current(invkernel,nvs[i,:,:],Jav)
 
 	clrs = cm.Blues(np.linspace(0.3,1.,nts))
-
 	for i in range(nts):
 		plt.plot(sites,nvs[i,0,:],color=clrs[i])
 	plt.xlabel(r'$x$ [$\mu$m]')
@@ -224,19 +218,13 @@ def main():
 	plt.ylabel(r'$n_-$ [$\phi_0/\mu$m$^{2}$]')
 	plt.show()
 
-
 	clrs = cm.Purples(np.linspace(0.,1.,nts))
 	for i in range(nts):
-		plt.plot(sites,nvs[i,0,:] - nvs[i,1,:],color=clrs[i])
+		plt.plot(links,Js[i,:],color=clrs[i])
+	plt.axhline(Jp,color='gray',linestyle='dashed')
 	plt.xlabel(r'$x$ [$\mu$m]')
-	plt.ylabel(r'$\Delta n_v$ [$\phi_0/\mu$m$^{2}$]')
+	plt.ylabel(r'$J_s/J_c$')
 	plt.show()
-
-	plt.plot(times,np.sum(nvs[:,0,:]- nvs[:,1,:],axis=-1))
-	plt.xlabel(r'$t$ [ps]')
-	plt.ylabel(r'$\langle \Delta n_v\rangle$ [$\phi_0/\mu$m$^{2}$]')
-	plt.show()
-
 
 
 
